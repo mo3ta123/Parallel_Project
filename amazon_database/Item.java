3b0237ccf1c1b790095dae950c3aa63e5054c041
omarmok22 <81467545+omarmok22@users.noreply.github.com>
2022-06-27 00:53:48 +0200

    Update Item.java

--- amazon_database/Item.java
+++ amazon_database/Item.java
@@ -52,7 +52,7 @@ public class Item {
                 prestatement.setString(4,Integer.toString(Price));
                 prestatement.setString(5,Img_URL);
                 prestatement.executeUpdate();
-                  connection.close();
+                 
              }
         catch (SQLException ex){
             java.util.logging.Logger.getLogger(Item.class.getName()).log(java.util.logging.Level.SEVERE,null,ex);
@@ -61,26 +61,38 @@ public class Item {
  
       
       
- public void itemView(int Item_ID)
+      
+   public  Vector<HashMap<String,String>>  itemView(int Item_ID)
  {
-        
+    Vector<HashMap<String,String>> result=new Vector<>();    
     try
     {
-        prestatement=connection.prepareStatement("select* from  item where  (Item_ID=?)");
-        prestatement.setInt(1, Item_ID);
+        prestatement=connection.prepareStatement("select* from  items where  (Item_ID=?)");
+        prestatement.setString(1, Integer.toString(Item_ID));
         resultset=prestatement.executeQuery();
-        while(resultset.next())
+       while(resultset.next())
         {
-            System.out.println(resultset.getString(2));
+            HashMap<String,String>map=new HashMap<String,String>();
+            map.put(Items_COLS.Item_ID, resultset.getString(Items_COLS.Item_ID));
+            map.put(Items_COLS.Name, resultset.getString(Items_COLS.Name));
+            map.put(Items_COLS.Amount_available, resultset.getString(Items_COLS.Amount_available));
+            map.put(Items_COLS.category, resultset.getString(Items_COLS.category));
+            map.put(Items_COLS.Price, resultset.getString(Items_COLS.Price));
+            map.put(Items_COLS.Img_URL, resultset.getString(Items_COLS.Img_URL));
+            result.add(map);
         }
-        connection.close();
-     
+    
     }
      
     catch (SQLException ex)
     {
         java.util.logging.Logger.getLogger(Item.class.getName()).log(java.util.logging.Level.SEVERE,null,ex);
     }
+    return result;
+}   
+ 
+     
+   
 }
 public void itemUpdate(int Item_ID,int Amount_available)
 {
@@ -96,7 +108,7 @@ public void itemUpdate(int Item_ID,int Amount_available)
           
           
          
-        connection.close();
+      
     } 
     catch (SQLException ex)
     {
@@ -143,15 +155,18 @@ public void itemUpdate(int Item_ID,int Amount_available)
  public static void main(String args[])  //static method  
 {  
 Item item=new Item();
-//item.itemInsert(3, "cameraame", 10, "electronics", "very smart camera", "https");
 item.connect();
 //item.itemUpdate(3,3);
  Vector<HashMap<String,String>> items=item.item_home_view(1);
-        System.out.println(items.size());
+    
+        Vector<HashMap<String,String>> items=item.itemView(10);
+      
         for (int i=0;i<items.size();i++)
         {
            
-         System.out.println(items.get(i).get(Items_COLS.Name)+"\t"+items.get(i).get(Items_COLS.Price)+"\t"+items.get(i).get(Items_COLS.Item_ID));
+         System.out.println(items.get(i).get(Items_COLS.Name)+"\t"+items.get(i).get(Items_COLS.category)+"\t"+items.get(i).get(Items_COLS.Price)+"\t"+items.get(i).get(Items_COLS.Item_ID));
         }
+}  
+}
 }  
 }
