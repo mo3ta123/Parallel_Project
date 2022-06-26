d014c251ee8771d7d31b4b2568646ce6515dd340
mo3ta123 <84969331+mo3ta123@users.noreply.github.com>
2022-06-27 01:11:09 +0200

    Create DataBase.java

---
+++ amazon_database/DataBase.java
@@ -0,0 +1,34 @@
+package amazon_database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import amazon_database.*;
+import java.util.HashMap;
+import java.util.Vector;
+import amazon_database.ServerDBConst.*;
+
+public class DataBase {
+    Connection connection;
+    Transaction transaction;
+    Item item;
+    User user;
+    public boolean connect(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection=DriverManager.getConnection("jdbc:mysql://localhost/amazon","root","");
+              return true;
+        }
+            
+    catch (ClassNotFoundException ex){
+        java.util.logging.Logger.getLogger(Item.class.getName()).log(java.util.logging.Level.SEVERE,null,ex);
+        return false;
+    }
+     catch (SQLException ex){
+        java.util.logging.Logger.getLogger(Item.class.getName()).log(java.util.logging.Level.SEVERE,null,ex);
+         return false;
+    }
+    }
+}
