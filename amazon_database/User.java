6e0398f3daf7542a12e10799a5d7949cf383b9e0
7usseiny-eg <64532906+7usseiny-eg@users.noreply.github.com>
2022-06-29 11:51:15 +0200

    Update User.java

--- amazon_database/User.java
+++ amazon_database/User.java
@@ -41,24 +41,22 @@ public class User {
             System.out.println("ERROR: Insert Failure");
         }
     }
-    public void update_user(int u_ID, String u_Name, String u_Password, int u_Balance, String u_Phone){ //INSERTING AN EMPTY STRING WILL RESULT IN NO CHANGE
+    public void update_user(String u_Name, String u_Password, double u_Balance, String u_Phone){ //INSERTING AN EMPTY STRING WILL RESULT IN NO CHANGE
         try{
             //Preparing the names
-            prestatement = connection.prepareStatement("SELECT * FROM USER WHERE User_ID = ?");
-            prestatement.setString(1, Integer.toString(u_ID));
+            prestatement = connection.prepareStatement("SELECT * FROM USER WHERE Name = ?");
+            prestatement.setString(1, u_Name);
             resultset = prestatement.executeQuery();
             if(resultset.next()){
-                if("".equals(u_Name)) u_Name = resultset.getString(User_COLS.USER_NAME);
                 if("".equals(u_Password)) u_Password = resultset.getString(User_COLS.USER_PASS);
                 if(u_Balance < 0) u_Balance = resultset.getInt(User_COLS.USER_BAL);
                 if("".equals(u_Phone)) u_Phone = resultset.getString(User_COLS.USER_PHONE);
             }
-            prestatement = connection.prepareStatement("UPDATE USER SET Name = ?, Password = ?, Phone = ?, Balance = ? WHERE User_ID = ?");
-            prestatement.setString(1, u_Name);
-            prestatement.setString(2, u_Password);
-            prestatement.setString(3, u_Phone);
-            prestatement.setString(4, Integer.toString(u_Balance));
-            prestatement.setString(5, Integer.toString(u_ID));
+            prestatement = connection.prepareStatement("UPDATE USER SET Password = ?, Phone = ?, Balance = ? WHERE Name = ?");
+            prestatement.setString(1, u_Password);
+            prestatement.setString(2, u_Phone);
+            prestatement.setString(3, Double.toString(u_Balance));
+            prestatement.setString(4, u_Name);
             prestatement.executeUpdate();
         }
         catch(SQLException ex){
@@ -101,28 +99,26 @@ public class User {
             }
             return result;
     }
-    public Vector<HashMap<String,String>> output_user(int u_ID){
-        Vector<HashMap<String,String>> result=new Vector<>();
+    public HashMap<String,String> output_user(String u_Name){
+        HashMap<String, String> map = new HashMap<String, String>();
         try{
-            prestatement = connection.prepareStatement("SELECT * FROM User WHERE User_ID = ?");
-            prestatement.setString(1, Integer.toString(u_ID));
+            prestatement = connection.prepareStatement("SELECT * FROM User WHERE Name = ?");
+            prestatement.setString(1, u_Name);
             resultset = prestatement.executeQuery();
             if(resultset.next()){
-                HashMap<String, String> map = new HashMap<String, String>();
-                map.put(User_COLS.USER_ID, resultset.getString(User_COLS.USER_ID));
                 map.put(User_COLS.USER_BAL, resultset.getString(User_COLS.USER_BAL));
                 map.put(User_COLS.USER_NAME, resultset.getString(User_COLS.USER_NAME));
                 map.put(User_COLS.USER_PASS, resultset.getString(User_COLS.USER_PASS));
                 map.put(User_COLS.USER_PHONE, resultset.getString(User_COLS.USER_PHONE));
                 map.put(User_COLS.USER_TYPE, resultset.getString(User_COLS.USER_TYPE));
-                result.add(map);
+                map.put(User_COLS.USER_EMAIL, resultset.getString(User_COLS.USER_EMAIL));
             }
-            return result;
+            return map;
         }
         catch(SQLException ex){
             //noting
         }
-        return result;
+        return map;
     }
 }
     
