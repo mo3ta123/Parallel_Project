ac34f991151b4976addcbc3e1e21742240bc3349
MarwanELMasry <85262373+MarwanELMasry@users.noreply.github.com>
2022-06-30 17:17:19 +0200

    Creating Server_Sercice Class

---
+++ server/Server_Service.java
@@ -0,0 +1,15 @@
+package server;
+
+import amazon_database.DataBase;
+import java.util.HashMap;
+import java.util.Vector;
+
+public class Server_Service
+{
+    DataBase DB ;
+    
+    Server_Service(DataBase DB)
+    {
+        this.DB = DB;
+    }
+}
