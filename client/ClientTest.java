2e26479cc4d477a621331e2ca628626b1cebbce4
AlaaMedhatMahmoud <85263585+AlaaMedhatMahmoud@users.noreply.github.com>
2022-06-30 17:15:54 +0200

    Add files via upload
    ClientTest Class upload

---
+++ client/ClientTest.java
@@ -0,0 +1,13 @@
+package client;
+
+import amazon_database.ServerDBConst.Holds_COLS;
+import amazon_database.ServerDBConst.Items_COLS;
+import amazon_database.ServerDBConst.Transaction_COLS;
+import amazon_database.ServerDBConst.User_COLS;
+import java.util.HashMap;
+import java.util.Vector;
+
+public class ClientTest {
+
+
+}
