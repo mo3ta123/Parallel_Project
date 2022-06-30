9c6cdfc3c11d71289c55c219cd4785dc48e123b6
OmarReda3 <84818274+OmarReda3@users.noreply.github.com>
2022-06-30 17:17:19 +0200

    Create Server Class

---
+++ server/Server.java
@@ -0,0 +1,34 @@
+package server;
+
+import amazon_database.DataBase;
+
+import java.io.EOFException;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server implements Runnable
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
