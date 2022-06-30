f67a6c6699fd4ecd0670cd5f48f5ee7c94322bd9
OmarReda3 <84818274+OmarReda3@users.noreply.github.com>
2022-06-30 17:18:34 +0200

    Create Server Test Class

---
+++ server/ServerTest.java
@@ -0,0 +1,13 @@
+package server;
+
+import amazon_database.DataBase;
+import amazon_database.ServerDBConst;
+import java.util.HashMap;
+import java.util.Vector;
+
+public class ServerTest {
+
+
+
+
+}
