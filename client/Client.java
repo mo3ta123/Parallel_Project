fc2cbfa97a700391c90b7b5da2e58dd9513a31fd
OmarReda3 <84818274+OmarReda3@users.noreply.github.com>
2022-07-01 02:42:01 +0200

    Adding setIP_ADDRESS function

--- client/Client.java
+++ client/Client.java
@@ -9,6 +9,25 @@ public class Client
 {
     String IP_ADDRESS;
 
+    private  void setIP_ADDRESS()
+    {
+
+        File file = new File("./src/client/IP_Address.txt");
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String str;
+
+            str = br.readLine();
+            IP_ADDRESS = str;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    
+    
     
 }
 
