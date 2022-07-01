12cc80fa9c145b83afcab9ce8560bf2593d1950f
AlaaMedhatMahmoud <85263585+AlaaMedhatMahmoud@users.noreply.github.com>
2022-07-01 02:49:25 +0200

    search function at client

--- client/Client.java
+++ client/Client.java
@@ -269,7 +269,42 @@ public void depositCash(String userName,double value)
 
     }
 
+public Vector<HashMap<String,String>> search(String searchString)
+    {
+        setIP_ADDRESS();
+        Vector<HashMap<String,String>> searchedOutput= new Vector<>();
+        try
+        {
+            // Setup Communication
+            Socket c = new Socket(IP_ADDRESS, 1234);
+            DataOutputStream dos = new DataOutputStream(c.getOutputStream());
+            DataInputStream dis = new DataInputStream(c.getInputStream());
+            ObjectOutputStream output = new ObjectOutputStream(dos);
+            ObjectInputStream input = new ObjectInputStream(dis);
 
+            // Ask server for signUp services
+            dos.writeUTF("search");
+
+            // Send username
+            dos.writeUTF(searchString);
+
+            // Wait for result
+            searchedOutput = (Vector<HashMap<String,String>>) input.readObject();
+
+            // Close Communication
+            dos.close();
+            dis.close();
+            c.close();
+
+        } catch (IOException  ex)
+        {
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return searchedOutput;
+    }
 
     
 }
