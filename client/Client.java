76bb8a24a4617e472a5548287275cfa89955904b
AlaaMedhatMahmoud <85263585+AlaaMedhatMahmoud@users.noreply.github.com>
2022-07-01 02:42:53 +0200

    Update Client.java
    login function at client

--- client/Client.java
+++ client/Client.java
@@ -26,7 +26,52 @@ public class Client
         }
     }
     
-    
+    public boolean login(String userName , String password)
+    {
+        setIP_ADDRESS();
+
+        String result = "invalidLogin";
+
+        try
+        {
+            // Setup Communication
+            Socket c = new Socket(IP_ADDRESS, 1234);
+            DataOutputStream dos = new DataOutputStream(c.getOutputStream());
+            DataInputStream dis = new DataInputStream(c.getInputStream());
+            ObjectOutputStream output = new ObjectOutputStream(dos);
+            ObjectInputStream input = new ObjectInputStream(dis);
+
+            // Ask server for login services
+            dos.writeUTF("login");
+
+            // Send username and password
+            dos.writeUTF(userName);
+            dos.writeUTF(password);
+
+            // Wait for result
+            result = dis.readUTF();
+
+
+            // Close Communication
+            dos.close();
+            dis.close();
+            c.close();
+
+        } catch (IOException  ex)
+        {
+
+        }
+
+        if ( result.equals("validLogin") )
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
     
     
 }
