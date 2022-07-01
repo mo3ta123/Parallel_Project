681a8d166e6c8e659b219f42222762e4ebee3a88
MarwanELMasry <85262373+MarwanELMasry@users.noreply.github.com>
2022-07-01 02:43:36 +0200

    Adding Signup Service in Client Class

--- client/Client.java
+++ client/Client.java
@@ -72,7 +72,54 @@ public class Client
         }
     }
 
-    
+        public boolean signUp(String userName , String password , String email , String phoneNumber)
+    {
+        setIP_ADDRESS();
+        String result = "invalidSignUp";
+
+        try
+        {
+            // Setup Communication
+            Socket c = new Socket(IP_ADDRESS, 1234);
+            DataOutputStream dos = new DataOutputStream(c.getOutputStream());
+            DataInputStream dis = new DataInputStream(c.getInputStream());
+            ObjectOutputStream output = new ObjectOutputStream(dos);
+            ObjectInputStream input = new ObjectInputStream(dis);
+
+            // Ask server for signUp services
+            dos.writeUTF("signUp");
+
+            // Send username and password
+            dos.writeUTF(userName);
+            dos.writeUTF(password);
+            dos.writeUTF(email);
+            dos.writeUTF(phoneNumber);
+
+            // Wait for result
+            result = dis.readUTF();
+
+
+            // Close Communication
+            dos.close();
+            dis.close();
+            c.close();
+
+        } catch (IOException  ex)
+        {
+
+        }
+
+        if ( result.equals("validSignUp") )
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+
     
 }
 
