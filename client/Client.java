b22e73615a41fc7ca44718f04f0ebd69efd34b1f
OmarReda3 <84818274+OmarReda3@users.noreply.github.com>
2022-07-01 02:44:46 +0200

    Update Client.java

--- client/Client.java
+++ client/Client.java
@@ -119,6 +119,48 @@ public class Client
         }
 
     }
+    
+    
+    public Vector<HashMap<String,String>> getInitialItems()
+    {
+        setIP_ADDRESS();
+        Vector<HashMap<String,String>> initialItems = new Vector<>();
+        try
+        {
+            // Setup Communication
+            Socket c = new Socket(IP_ADDRESS, 1234);
+            DataOutputStream dos = new DataOutputStream(c.getOutputStream());
+            DataInputStream dis = new DataInputStream(c.getInputStream());
+            ObjectOutputStream output = new ObjectOutputStream(dos);
+            ObjectInputStream input = new ObjectInputStream(dis);
+
+            // Ask server for signUp services
+            dos.writeUTF("getInitialItems");
+
+
+
+            // Wait for result
+            initialItems = (Vector<HashMap<String,String>>) input.readObject();
+
+
+            // Close Communication
+            dos.close();
+            dis.close();
+            c.close();
+
+        } catch (IOException  ex)
+        {
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return initialItems;
+    }
+
+    
+    
+    
 
     
 }
