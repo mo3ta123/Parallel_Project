337b62e9833449fe887f9d0d20dce0756e52cac9
AlaaMedhatMahmoud <85263585+AlaaMedhatMahmoud@users.noreply.github.com>
2022-07-01 02:52:22 +0200

    getTransactionHistory function at client

--- client/Client.java
+++ client/Client.java
@@ -394,6 +394,45 @@ public Vector<HashMap<String,String>> search(String searchString)
         return result;
 
     }
+    
+    
+    public  Vector<HashMap<String,String>> getTransactionHistory(String userName)
+    {
+        setIP_ADDRESS();
+        Vector<HashMap<String,String>> TransactionHistory = new Vector<>();
+        try
+        {
+            // Setup Communication
+            Socket c = new Socket(IP_ADDRESS, 1234);
+            DataOutputStream dos = new DataOutputStream(c.getOutputStream());
+            DataInputStream dis = new DataInputStream(c.getInputStream());
+            ObjectOutputStream output = new ObjectOutputStream(dos);
+            ObjectInputStream input = new ObjectInputStream(dis);
+
+            // Ask server for signUp services
+            dos.writeUTF("getTransactionHistory");
+
+            // Send username
+            dos.writeUTF(userName);
+
+            // Wait for result
+            TransactionHistory = (Vector<HashMap<String,String>>) input.readObject();
+
+
+            // Close Communication
+            dos.close();
+            dis.close();
+            c.close();
+
+        } catch (IOException  ex)
+        {
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return TransactionHistory;
+    }
 
 
 }
