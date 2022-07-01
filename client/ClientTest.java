6aa5977d7e6f47406a607bc115d5799a55205874
AlaaMedhatMahmoud <85263585+AlaaMedhatMahmoud@users.noreply.github.com>
2022-07-01 03:21:04 +0200

    ClientTest: login , singup , Editing Items

--- client/ClientTest.java
+++ client/ClientTest.java
@@ -8,6 +8,51 @@ import java.util.HashMap;
 import java.util.Vector;
 
 public class ClientTest {
+  
+  public static void main(String[] args) {
+        Client obj = new Client();
+
+
+        /* Test cases for login && singup /
+
+
+        //False
+        System.out.println("First Trial login result  = " + obj.login("user", "abcda"));
+
+         //True
+        System.out.println("First Trial signUp result  = " + obj.signUp("user", "abcda","Email1","01099501184"));
+
+        // True
+        System.out.println("Second Trial login result = " + obj.login("user", "abcda"));
+
+         //False
+        System.out.println("First Trial signUp result  = " + obj.signUp("user", "abcda","Email1","01099501184"));
+
+        System.out.println();
+        System.out.println();
+
+
+
+        / Test cases for Editing Items */
+
+        //Test1
+
+        // True
+        System.out.println(obj.editItemAtCart("user",1,1));
+
+        // True
+        System.out.println(obj.editItemAtCart("user",2,10)); 
+
+        // False
+        System.out.println(obj.editItemAtCart("user",1,22)); 
+
+        // True
+        //System.out.println(obj.editItemAtCart("user",1,0));
+
+        System.out.println();
+        System.out.println();
+    
+  }
 
 
 }
