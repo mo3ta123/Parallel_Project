01fe6cfbcbcac27ac32c9017b01af34b3ce407a9
MarwanELMasry <85262373+MarwanELMasry@users.noreply.github.com>
2022-07-01 03:34:54 +0200

    Needed Stubs from database

--- amazon_database_STUB/DataBase.java
+++ amazon_database_STUB/DataBase.java
@@ -52,5 +52,72 @@ public class DataBase
         return items;
     }
                   
-                  
+         // Stubs for deposit , save this deposit in transaction history to be viewed later
+    public synchronized void depositBalance(String userName, double amount)
+    {
+        System.out.println("deposit :"+amount);
+    }
+
+    // Stub for get account INFO, get all User table of specific user name
+    public synchronized Vector<HashMap<String,String>> getAccountInfo(String userName)
+    {
+        Vector<HashMap<String,String>> items = new Vector<HashMap<String,String>>();
+
+        HashMap<String,String> item1 = new HashMap<String,String>();
+        item1.put("Item_ID", "1");
+        item1.put("Name", "REDA");
+        item1.put("Amount_available", "20");
+        item1.put("category", "'Electronics'");
+        item1.put("Img_URL", "https://egyptlaptop.com/images/detailed/31/1594281073_IMG_1384580.jpg");
+        item1.put("Price", "5000");
+        item1.put(ServerDBConst.Cart_COLS.Amount, "100");
+
+        items.add(item1);
+
+        return items;
+    }
+
+
+    // Stubs for GetMYCArt
+
+   /*
+    *  It Returns :
+    *
+    * SELECT Item_IDو Name وAmount_available,category ,Img_URL, Price , Amount
+    * FROM   Item, cart
+    * WHERE  Item.Item_ID = cart.Item_ID AND
+    *       user_name = function parameter (userName)
+    *
+    *
+    */
+    public synchronized Vector<HashMap<String,String>> getMyCart(String userName)
+    {
+        Vector<HashMap<String,String>> items = new Vector<HashMap<String,String>>();
+
+        HashMap<String,String> item1 = new HashMap<String,String>();
+        item1.put("Item_ID", "1");
+        item1.put("Name", "Camera");
+        item1.put("Amount_available", "20");
+        item1.put("category", "'Electronics'");
+        item1.put("Img_URL", "https://egyptlaptop.com/images/detailed/31/1594281073_IMG_1384580.jpg");
+        item1.put("Price", "5000");
+        item1.put(ServerDBConst.Cart_COLS.Amount, "100");
+
+        items.add(item1);
+
+        HashMap<String,String> item2 = new HashMap<String,String>();
+        item2.put("Item_ID", "2");
+        item2.put("Name", "Phone");
+        item2.put("Amount_available", "10");
+        item2.put("category", "'Electronics'");
+        item2.put("Img_URL", "https://media.4rgos.it/i/Argos/9520608_R_Z001A?w=750&h=440&qlt=70");
+        item2.put("Price", "6000");
+        item2.put(ServerDBConst.Cart_COLS.Amount, "100");
+
+        items.add(item2);
+
+
+        return items;
+    }
+             
 }
